35abdd132f26ab4ed9b336a2e51d05bd1be65305
Jonathanjojo <jonathanchristopher1199@gmail.com>
2019-03-11 19:09:15 +0700

    Fixed positioning on cards

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -8,9 +8,9 @@ const StyledButton = styled.button`
     text-decoration: none;
     margin: 1em;
     min-width: 200px;
-    padding: 1em 2.5em;
+    padding: 1em 3em;
     border: 1px solid ${props => props.dark ? "#9EA8C7" : "#000011"};
-    transition: all 0.3s ease-out;
+    transition: all 0.5s ease-out;
 
     & {
         display: inline-block;
--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -1,47 +1,38 @@
 import React from 'react';
-import {
-    Card,
-    CardText,
-    CardBody,
-    CardTitle,
-} from 'reactstrap';
-
 import styled from 'styled-components';
 import gitLogo from "../../icons/git.svg";
 
 const SpacedCard = styled.div`
-    margin: 1em 0.5em;
+    margin: 0.5em;
 `;
 
 const ImageWrapper = styled.div`
-    // position: relative;
+    position: relative;
     width:27px;
+    display: flex;
+    justify-content: middle;
     background-color: ${props => props.dark ? "#000011" : "#9EA8C7"};;
     border-radius: 50%;
-    float: right;
     margin: 0.5em 0.5em 0.5em auto;
+    text-align: center;
 
     &:before{
         content: "";
         display: block;
-        padding-top: 100%;
+        padding-top: 100%; 
     }
 `;
 
-const WrappedImage = styled.div`
-    // position:  absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
+const WrappedImage = styled.img`
+
 `;
 
 const CardStyle = {
+    textAlign: "left",
     backgroundColor: "#091628",
     borderRadius: 0,
     borderColor: "#091628",
-    paddingTop: 0,
-    paddingBottom: 25,
+    padding: 25,
     color: "#9EA8C7",
 }
 
@@ -52,38 +43,38 @@ class ProjectCard extends React.Component {
         if (this.props.gitrepo !== "") {
             link = 
                 <a href={this.props.gitrepo}>
-                    <img
+                    <WrappedImage
                         src={gitLogo}
                         alt="Repo"
-                        style={{ width: 16, margin: 2 }}
+                        style={{ width: 20, zIndex:2, margin:3}}
                     />
                 </a>;
         } else {
             link = 
-                <img
+                <WrappedImage
                     src={gitLogo}
                     alt="Repo"
-                    style={{ width: 16, margin: 2 }}
+                    style={{ width: 20, zIndex:2, margin:3 }}
                 />
         }
         return (
             <SpacedCard>
-                <div style={CardStyle}>
-                    {/* <Card className="project-card" style={CardStyle}>
+                <div className="project-card draw" style={CardStyle}>
+                    <div>
                         <ImageWrapper dark={this.props.dark}>
-                            <WrappedImage>
-                                {link}
-                            </WrappedImage>
+                            {link}
                         </ImageWrapper>
-                        <CardBody style={{ paddingTop: 0, textAlign: "left"}}>
-                            <CardTitle>
-                                <h3>
-                                    {this.props.title}
-                                </h3>
-                            </CardTitle>
-                            <CardText>{this.props.children}</CardText>
-                        </CardBody>
-                    </Card> */}
+                    </div>
+                    <div>
+                        <h3>
+                            {this.props.title}
+                        </h3>
+                    </div>
+                    <div>
+                        <p>
+                            {this.props.children}
+                        </p>
+                    </div>
                 </div>
             </SpacedCard>
         );

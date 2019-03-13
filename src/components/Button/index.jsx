001f6dd5ab8ac110ce193e9f013b2f70f789e648
Jonathanjojo <jonathanchristopher1199@gmail.com>
2019-03-13 13:48:39 +0700

    Fix button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -12,7 +12,7 @@ const StyledButtonLink = styled(Link)`
     padding: 1em 3em;
     border: 1px solid ${props => props.dark ? "#9EA8C7" : "#000011"};
     transition: all 0.5s ease-out;
-
+    cursor: pointer;
     & {
         display: inline-block;
     }
@@ -28,38 +28,6 @@ const StyledButtonLink = styled(Link)`
         transform: scale(0,1);
     }
       
-    &:hover:after {
-        width: 100%;
-        transform: scale(1,1);
-    }
-`;
-
-const StyledButtonA = styled.a`
-    background: ${props => props.dark ? "#000011" : "#9EA8C7"};
-    color: ${props => props.dark ? "#9EA8C7" : "#000011"};
-    font-size: 1em;
-    text-decoration: none;
-    margin: 1em;
-    min-width: 200px;
-    padding: 1em 3em;
-    border: 1px solid ${props => props.dark ? "#9EA8C7" : "#000011"};
-    transition: all 0.5s ease-out;
-
-    & {
-        display: inline-block;
-    }
-    
-    &:after {
-        content: '';
-        width: 100%;
-        height: 2px;
-        display: block;
-        background: ${props => props.dark ? "#9EA8C7" : "#000011"};
-        transition: 0.5s;
-        transform-origin: 50% 50%;
-        transform: scale(0,1);
-    }
-    
     &:hover:after {
         width: 100%;
         transform: scale(1,1);
@@ -69,33 +37,21 @@ const StyledButtonA = styled.a`
 
 class Button extends React.Component {
     render() {
-        let link;
-        if (this.props.external) {
-            link = 
-                <StyledButtonA
-                    dark={this.props.dark} 
-                    to={this.props.to} 
-                    spy={this.props.spy}
-                    smooth={this.props.smooth} 
-                    offset={this.props.offset} 
-                    duration={this.props.duration}
-                >
-                    {this.props.children}
-                </StyledButtonA>
-        } else {
-            link = 
-                <StyledButtonLink
-                    dark={this.props.dark} 
-                    href={this.props.href}
-                    style={this.props.style}
-                    target={this.props.target}
-                    rel={this.props.rel}
-                >
-                    {this.props.children}
-                </StyledButtonLink>
-        }
         return (
-            link
+            <StyledButtonLink
+                dark={this.props.dark} 
+                to={this.props.to} 
+                spy={this.props.spy}
+                smooth={this.props.smooth} 
+                offset={this.props.offset} 
+                duration={this.props.duration}
+                href={this.props.href}
+                style={this.props.style}
+                target={this.props.target}
+                rel={this.props.rel}
+            >
+                {this.props.children}
+            </StyledButtonLink>
         )
     }
 }

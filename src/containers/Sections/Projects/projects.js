75350d5463fcf413393f623d6f73f0fd08bef324
jonathanjojo <jonathanchristopher1199@gmail.com>
2019-07-09 22:14:02 +0700

    Update projects content and description

--- src/containers/Sections/Projects/projects.js
+++ src/containers/Sections/Projects/projects.js
@@ -6,8 +6,8 @@ const projects = [
             // eslint-disable-next-line
             "ReversAI is an AI-Powered reversi player agent that is available online using ReactJS framework. \
             Using the MiniMax Algorithm with Alpha-Beta Pruning, the agent will come up with a move maximizing its success \
-            even if the opponent is playing optimally. Not only our users get to fight a formidable foe, \
-            they will also be indulged with the delicate UI and difficulty levels to satisfy our users eagerness to play. \
+            even if the opponent is playing optimally. Not only our users get to fight a formidable foe, but \
+            they will also be indulged with the delicate UI and difficulty levels to satisfy our user's eagerness to play. \
             The live application can be accessed through https://reversai.netlify.com.", 
         dark: false
     },
@@ -30,7 +30,7 @@ const projects = [
             // eslint-disable-next-line
             "This very web you are visiting is one of my own personal project. It main goal is to display my interests and abilities for you all to know \
             me better. It kickstarted my experience on developing a frontend with React JS. Feel free to explore this web and get to know me \
-            if you are interested, it would be a great honor to know each other and I can't wait to know you as well.",
+            if you are interested. It would be a great experience and honor to know each other and I can't wait to know you as well.",
         dark: false
     },
     {
@@ -39,21 +39,21 @@ const projects = [
         content:
             // eslint-disable-next-line
             "ToysRent is a toy rental service simulator group project built with Django and pure SQL only. \
-            This project is an actual implementation of SQL comprehension in web developing. \
-            Prior to the creation of the front end side of this project, an SQL database was built from scratch - from the tables, relations, data, \
+            This project is an actual implementation of SQL comprehension in web developing using PostgreSQL database. \
+            Prior to the creation of the front end side of this project, the database was built from scratch - from the tables, relations, data, \
             triggers, to stored procedures. On top of that base, we created a Django project to enable users to manipulate the database with respected \
             authorizations through user friendly and responsive UI.",
         dark: false
     },
     {
-        title: "CareShare",
-        gitrepo: "https://gitlab.com/Jonathanjojo/careshare",
+        title: "Minimalist Portfolio CMS",
+        gitrepo: "https://github.com/Jonathanjojo19/PortfolioCMS",
         content: 
             // eslint-disable-next-line
-            "CareShare is a crowdfunding application simulator. It was an assigned project for Web Development Course I took in college and in fact on of\
-            the first collaborative Django project that started my experience with Django. It was built with the Django framework, HTML + CSS, JS and Ajax, \
-            and with the help of Google Auth for authentications of users. During the making of this project, I managed and led a group of four \
-            on completing the task with satisfying results and delivered quality." ,
+            "This is a Django based minimalist portfolio page with an admin page to edit the contents displayed in the main page of the site. \
+            The owner can set and display various information such as owner name, introduction message, page title, LinkedIn profile link, email, \
+            phone number, up to owner's projects with description and URL to the project. The updated values from the admin page will be displayed right away \
+            in the main page of this application for visitors to see." ,
         dark: false
     },
     {
@@ -62,7 +62,7 @@ const projects = [
         content: 
             // eslint-disable-next-line
             "StoreManager is a project of 2 people, which I was assigned to and worked on Web Store Manager's back end side: \
-            providing API views and permissions handling. StoreManager's backend is built on a robust DjangoREST API services. \
+            providing API views and permissions handling. StoreManager's backend is built on a robust Django-REST API services. \
             It is designed to be a usable template for online stores to monitor, \
             display, and manage their own online store through a website: admin dashboard and customer views.",
         dark: false
